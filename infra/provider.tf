61e839e5bded45b84a968c1645a023724553acc2
Chethan KP <chethan.k.p.-nd@disney.com>
2024-02-12 19:54:28 +0530

    region added

--- infra/provider.tf
+++ infra/provider.tf
@@ -9,7 +9,7 @@ terraform {
   backend "s3" {
     bucket = "dev-proj-1-remote-state-bucket-kpc-project"
     key    = "devops-project-1/terraform.tfstate"
-    region = "eu-central-1"
+    region = "us-east-1"
   }
 }
 
